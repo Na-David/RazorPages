4fd270bf206f1a134a17bd965085d818d6425b1e
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-06-20 23:28:46 +1200

    Json Deserialize method

---
+++ David/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,22 @@
+﻿using David.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace David.Services
+{
+	public class PortfolioServiceJsonFile
+	{
+		public IEnumerable<Portfolio> GetPortfolios()
+		{
+			var jsonFileName = @"C:\Users\hna20\Desktop\ASP.NET\WebApp_Razor\David\wwwroot\Portfolios\portfolios.json";
+
+			using (var jsonFileReader = File.OpenText(jsonFileName))
+			{
+				var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+				var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
+				return portfolios;
+			}
+		}
+	}
+}
