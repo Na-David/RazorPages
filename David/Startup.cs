36c208a169281b4465dc896a735d38f835dd52f7
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-06-21 00:49:26 +1200

    Dependency Injection(transient, IWebHostEnvironment)

--- David/Startup.cs
+++ David/Startup.cs
@@ -1,3 +1,4 @@
+using David.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,7 @@ namespace David
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<PortfolioServiceJsonFile>();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
