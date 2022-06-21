e640a60315cc73f5b37da8f45213d913a4b90675
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-06-22 01:20:06 +1200

    Injection differences practice(code behind)

--- David/Pages/Portfolios/Index.cshtml.cs
+++ David/Pages/Portfolios/Index.cshtml.cs
@@ -1,12 +1,24 @@
+using David.Models;
+using David.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace David.Pages.Portfolios
 {
 	public class IndexModel : PageModel
 	{
-		public void OnGet()
-		{
+        private readonly PortfolioServiceJsonFile _service;
 
+        public IndexModel(PortfolioServiceJsonFile service)
+        {
+            this._service = service;
+        }
+
+        public IEnumerable<Portfolio> Portfolios { get; private set; }
+
+        public void OnGet()
+		{
+            Portfolios = _service.GetPortfolios();
 		}
 	}
 }
---
+++ David/Pages/Portfolios/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace David.Pages.Portfolios
+{
+	public class ListModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
+}
