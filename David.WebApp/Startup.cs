31de1ef251f4bfab851d71e68509c4b8a3a861b7
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-07-17 23:55:31 +1200

    Details changed

--- David.WebApp/Startup.cs
+++ David.WebApp/Startup.cs
@@ -50,7 +50,7 @@ namespace David.WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            //Below these are Middleware
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
